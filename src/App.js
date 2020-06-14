61e8c60430f453664d0e136d4ef27b555e8a6546
Connor Foster <foster.clee@gmail.com>
2020-06-14 16:39:10 -0500

    Added in logo & background color to header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import './App.css';
-
+import Header from './components/HeaderComponent'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </React.Fragment>
     );
   }
 }
