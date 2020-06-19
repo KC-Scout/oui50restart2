5f3570b01c94056f31a83733e73c03879daaf28d
Peyton <pcopcoeco@gmail.com>
2020-06-18 19:17:13 -0500

    title fix and icon fix

--- src/components/ArticleInfoComponent.js
+++ src/components/ArticleInfoComponent.js
@@ -5,7 +5,7 @@ function RenderArticle({ article }) {
     return (
         <div className="col m-5">
             <Card>
-                <CardImg top src={article.image} alt={article.name} />
+                <CardImg top src={article.image} alt={article.title} />
                 <CardBody>
                     <CardText>{article.contents}</CardText>
                 </CardBody>
@@ -18,8 +18,8 @@ function ArticleInfo({ article }) {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <h2>{article.name}</h2>
+                    <div className="col mt-2">
+                        <h2>{article.title}</h2>
                         <hr />
                     </div>
                 </div>
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -18,8 +18,8 @@ function Footer(props) {
                     </div>
                     <div className="col-md-4 text-center">
                         <h5 id="footer">Follow Us!</h5>
-                        <a className="btn btn-social-icon btn-twitter btn-light" href="http://twitter.com/oui_50"><i className="fa fa-twitter fa-lg" aria-hidden="true" /></a>{' '}
-                        <a className="btn btn-social-icon btn-google btn-light" href="https://www.youtube.com/channel/UCEvkWG11yDDvEkwaUJbRIRg"><i className="fa fa-youtube" /></a>
+                        <a className="btn btn-social-icon btn-twitter btn-light" href="http://twitter.com/oui_50"><i className="fa fa-twitter fa-lg" aria-label="Twitter Logo" /></a>{' '}
+                        <a className="btn btn-social-icon btn-google btn-light" href="https://www.youtube.com/channel/UCEvkWG11yDDvEkwaUJbRIRg"><i className="fa fa-youtube fa-lg" /></a>
                     </div>
                 </div>
             </div>
